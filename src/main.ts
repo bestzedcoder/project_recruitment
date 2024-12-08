a3ed345d289647d48d02aa702288bca307139d58
bestzedcoder <150442914+bestzedcoder@users.noreply.github.com>
2024-12-09 00:25:33 +0700

    config versioning

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from "./app.module";
 import { NestExpressApplication } from "@nestjs/platform-express";
 import { join } from "path";
 import { ConfigService } from "@nestjs/config";
-import { ValidationPipe } from "@nestjs/common";
+import { ValidationPipe, VersioningType } from "@nestjs/common";
 import { JwtAuthGuard } from "./auth/guard/jwt-auth.guard";
 import { TransformInterceptor } from "./core/transform.interceptor";
 
@@ -22,6 +22,12 @@ async function bootstrap() {
     origin: "*",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     preflightContinue: false,
+  });
+  // config versioning
+  app.setGlobalPrefix("api");
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: ["1", "2"],
   });
   await app.listen(configService.get("PORT"));
 }
